6cc6b862477582d00d401c7730ae5fa798124a8a
koshikawa <koshikawa@ppworks.jp>
2014-07-16 10:07:42 +0900

    Credit view

--- SushiCamera/SushiCamera.m
+++ SushiCamera/SushiCamera.m
@@ -8,9 +8,9 @@
 
 #import "SushiCamera.h"
 
-@interface SushiCamera ()
+@interface SushiCamera ()<UIWebViewDelegate>
 @property (nonatomic) UIButton *creditButton;
-
+@property (nonatomic) UIWebView *creditView;
 @end
 
 @implementation SushiCamera
@@ -26,43 +26,77 @@
     return self;
 }
 
-- (void)didReceiveMemoryWarning
+- (void)viewDidAppear:(BOOL)animated
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [super viewDidAppear:animated];
+    [self setupCredit];
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    if (!self.creditView.hidden) {
+        self.creditView.hidden = YES;
+    }
 }
 
 - (void)drawControls
 {
     [super drawControls];
-    [self setupCreditButton];
+    [self changeCreditPosition];
 }
 
-- (void)setupCreditButton
+- (void)setupCredit
 {
-    self.creditButton = nil;
     self.creditButton = [[UIButton alloc] init];
+    [self.creditButton addTarget:self action:@selector(touchedCreditButton:) forControlEvents:UIControlEventTouchUpInside];
     [self.creditButton setImage:[UIImage imageNamed: @"sushiyuki_27"] forState:UIControlStateNormal];
     
     self.creditButton.bounds = CGRectMake(0, 0, 40, 40);
     self.creditButton.backgroundColor = [UIColor colorWithWhite:1 alpha:0.6];
-    self.creditButton.alpha = 0.6;
-    
+
     self.creditButton.layer.shouldRasterize = YES;
     self.creditButton.layer.rasterizationScale = [UIScreen mainScreen].scale;
     self.creditButton.layer.cornerRadius = 4;
     
     self.creditButton.layer.borderColor = [UIColor lightGrayColor].CGColor;
     self.creditButton.layer.borderWidth = 0.5;
+    [self.view addSubview:self.creditButton];
     
+    self.creditView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 0, 300, 200)];
+    self.creditView.backgroundColor = [UIColor colorWithWhite:1 alpha:0.6];
+    self.creditView.opaque = NO;
+    self.creditView.hidden = YES;
+    self.creditView.delegate = self;
+    [self.view addSubview:self.creditView];
     
+    [self changeCreditPosition];
+}
+
+- (void)changeCreditPosition
+{
     if (UIInterfaceOrientationIsPortrait(self.interfaceOrientation)) {
         self.creditButton.center = CGPointMake(30, self.captureBtn.center.y);
-        //self.creditButton.center = CGPointMake(30, self.view.bounds.size.height - 28);
     } else {
         self.creditButton.center = CGPointMake(self.captureBtn.center.x, 30);
     }
-    [self.view addSubview:self.creditButton];
+    self.creditView.center = self.view.center;
+}
+
+- (void)touchedCreditButton:(id)sender
+{
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"credit" ofType:@"html"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL fileURLWithPath:path]];
+    [self.creditView loadRequest:request];
+    self.creditView.hidden = NO;
+}
+
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
+{
+    if (UIWebViewNavigationTypeLinkClicked == navigationType) {
+        [[UIApplication sharedApplication] openURL:[request URL]];
+        return NO;
+    }
+    return YES;
 }
 
 @end
--- SushiCamera/ViewController.m
+++ SushiCamera/ViewController.m
@@ -129,5 +129,4 @@
 - (void) simpleCamDidLoadCameraIntoView:(SimpleCam *)simpleCam {
     NSLog(@"Camera loaded ... ");
 }
-
 @end
